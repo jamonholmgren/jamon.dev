f3b340cb77cd9544286fadfdf9e66ec8e440770c
Jamon Holmgren <jamonholmgren@gmail.com>
2022-11-30 18:26:56 -0800

    move to build

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,9 @@
 # Constants -- change these to match your setup
 DOMAIN="qb.jamonholmgren.com"
 # The below are probably accurate if you have a default Ubuntu DigitalOcean droplet.
+# If you edit them, also update any references in qb64-watcher.service!
 SSH_USER="root"
 REMOTE_ROOT="/root"
 REMOTE_REPO_FOLDER="$REMOTE_ROOT/repo"
 REMOTE_QB64_FOLDER="$REMOTE_ROOT/qb64"
+REMOTE_BUILD_FOLDER="$REMOTE_ROOT/build"
